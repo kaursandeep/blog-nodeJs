711bf89909b10f5cb10734780715ae082cc475fa
sandeep dhaliwal <sandeepkaur452@gmail.com>
2017-06-09 23:59:54 +0000

    aa

--- server.js
+++ server.js
@@ -16,9 +16,14 @@ var express = require('express');
 //
 var router = express();
 var server = http.createServer(router);
-
+/*
+mongoose.connect('mongodb://root:76east@ds159371.mlab.com:59371/instagram-db');
+*/
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
+
+//app.listen(process.env.PORT, process.env.IP);
+
